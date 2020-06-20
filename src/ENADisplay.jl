423efdd720a1bb4b6d91095bf3c166f373321701
Bryan A. Knowles <snotskie@gmail.com>
2020-06-20 12:11:46 -0500

    adjust margins

--- src/ENADisplay.jl
+++ src/ENADisplay.jl
@@ -28,7 +28,7 @@ function Plots.plot(ena::ENAModel;
     showconfidence::Bool=false)
 
     # Plot
-    p = plot(leg=false, margin=20mm)
+    p = plot(leg=false, margin=10mm)
 
     # Artist (to choose the colors etc.)
     artist(ena, p) do networkColors,
