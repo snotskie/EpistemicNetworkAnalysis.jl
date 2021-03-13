5c9f56cf5ff39133ca1a53a4b9a7691d4c746cce
Bryan A. Knowles <snotskie@gmail.com>
2021-03-13 17:10:58 -0600

    LDA now uses MR for y in 2-group

--- src/LDARotation.jl
+++ src/LDARotation.jl
@@ -16,17 +16,47 @@ function rotate!(rotation::AbstractLDARotation, networkModel::DataFrame, unitMod
 
     ## Run the LDA
     ldaModel = projection(fit(MulticlassLDA, nc, X, y))
-    networkModel[!, :weight_x] = ldaModel[:, 1]
     if size(ldaModel, 2) >= 2
+        networkModel[!, :weight_x] = ldaModel[:, 1]
         networkModel[!, :weight_y] = ldaModel[:, 2]
     else
-        ## Find the first svd dim of the data orthogonal to the x weights, use these as the y weights
-        xAxis = Matrix{Float64}(unitModel[!, networkModel[!, :relationship]]) *
-        Matrix{Float64}(networkModel[!, [:weight_x]])
-        xAxis = xAxis .- mean(xAxis)
-        controlModel = DataFrame(xAxis)
-        pcaModel = projection(help_deflating_svd(networkModel, unitModel, controlModel))
-        networkModel[!, :weight_y] = pcaModel[:, 1]
+        ## Use MR1's x-axis as my approximate y-axis
+        groups = unique(metadata[!, rotation.groupVar])
+        rotate!(MeansRotation(rotation.groupVar, groups[1], groups[2]), networkModel, unitModel, metadata)
+        networkModel[!, :weight_y] = networkModel[!, :weight_x]
+        networkModel[!, :weight_x] = ldaModel[:, 1]
+
+        ## Normalize the weights for both axes
+        s = sqrt(sum(networkModel[!, :weight_x] .^ 2))
+        if s != 0
+            networkModel[!, :weight_x] /= s
+        end
+
+        s = sqrt(sum(networkModel[!, :weight_y] .^ 2))
+        if s != 0
+            networkModel[!, :weight_y] /= s
+        end
+
+        ## Orthogonalization: replace y weights with their rejection from the x weights
+        scalar = dot(networkModel[!, :weight_y], networkModel[!, :weight_x]) / dot(networkModel[!, :weight_x], networkModel[!, :weight_x])
+        networkModel[!, :weight_y] -= scalar * networkModel[!, :weight_x]
+
+        ## Re-normalize the weights for the y-axis
+        s = sqrt(sum(networkModel[!, :weight_y] .^ 2))
+        if s < 0.05
+            networkModel[!, :weight_y] .= 0
+            @warn "During the rotation step, the y axis was deflated to zero due to close correlation with the x axis."
+        elseif s != 0
+            networkModel[!, :weight_y] /= s
+        end
+        
+        # ## Find the first svd dim of the data orthogonal to the x weights, use these as the y weights
+        # xAxis = Matrix{Float64}(unitModel[!, networkModel[!, :relationship]]) *
+        # Matrix{Float64}(networkModel[!, [:weight_x]])
+        # xAxis = xAxis .- mean(xAxis)
+        # controlModel = DataFrame(xAxis)
+        # pcaModel = projection(help_deflating_svd(networkModel, unitModel, controlModel))
+        # networkModel[!, :weight_y] = pcaModel[:, 1]
     end
 
     ## Normalize the axis weights
