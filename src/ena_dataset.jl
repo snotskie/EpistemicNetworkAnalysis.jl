555ad76d09dfeeae2435ca5fafc5c87cb0a0bafc
Bryan A. Knowles <snotskie@gmail.com>
2021-10-21 15:02:01 -0500

    clean up

--- src/ena_dataset.jl
+++ src/ena_dataset.jl
@@ -1,8 +1,8 @@
 function ena_dataset(name::String)
-    if name in ["RS.data", "shakespeare.data"]
+    if name in ["shakespeare.data"]
         filename = joinpath(dirname(@__FILE__), "..", "data", "$(name).csv")
         return DataFrame(CSV.File(filename, missingstring="#N/A"))
     else
-        error("ena_dataset only works with ena_dataset(\"RS.data\") and ena_dataset(\"shakespeare.data\") right now. If you have your own dataset, you can load it with DataFrame(CSV.File(file_name_here, other_settings_here)). Thanks!")
+        error("ena_dataset only works with ena_dataset(\"shakespeare.data\") right now. If you have your own dataset, you can load it with DataFrame(CSV.File(file_name_here, other_settings_here)). Thanks!")
     end
 end
