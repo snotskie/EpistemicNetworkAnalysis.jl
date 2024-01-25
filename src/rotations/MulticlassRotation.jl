4dc5bd3c1fd5925b364b27798952d10e8f5d09c4
Mariah A. Knowles <snotskie@gmail.com>
2024-01-25 15:21:08 -0600

    MCMR had too many dimensions

--- src/rotations/MulticlassRotation.jl
+++ src/rotations/MulticlassRotation.jl
@@ -68,7 +68,7 @@ function rotate!(
     vecs = eigvecs(Sb)
 
     ## Add to the model
-    ns = size(vecs, 2)
+    ns = min(size(vecs, 2), length(groupNames)-1)
     embedding = similar(model.embedding, ns)
     for i in 1:ns
         embedding[i, :label] = "MCMR$(i)"
