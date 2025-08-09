8db996f586784fadfc49597643edf7f635756d16
Mariah A. Knowles <snotskie@gmail.com>
2025-08-09 08:57:07 -0500

    tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -37,7 +37,7 @@ end
 end
 
 # Test that each model/rotation combination runs
-models = [CodewiseENAModel, ENAModel, BiplotENAModel, DigraphENAModel]
+models = [ENAModel, BiplotENAModel, DigraphENAModel, CodewiseENAModel]
 rotations = [
     # expected xaxis label => rotation
     "SVD1" => SVDRotation(),
